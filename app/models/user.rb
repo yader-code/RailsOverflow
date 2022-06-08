41b49e3ed55bf1156edb62eb4cb44a813168e8bc
yader-code <jhadermoreno@hotmail.com>
2022-06-08 16:21:10 -0500

    PR comments applied, changes applied to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :keywords, presence: true
+
+  validates_length_of :name, minimum: 3
+
+  before_save :upcase_first_letters
+  before_save :remove_white_spaces_begin_and_end_of_name
+
+  private
+
+  def upcase_first_letters
+    full_name = name.split
+    self.name = ''
+    full_name.each { |word| self.name += "#{word.capitalize} " }
+  end
+
+  def remove_white_spaces_begin_and_end_of_name
+    self.name.strip!
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_192842) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_201246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "keywords"
-    t.string "name"
+    t.string "keywords", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
