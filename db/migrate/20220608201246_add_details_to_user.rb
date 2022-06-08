11727a6b796578e6171c02b880c63530b017f7b3
yader-code <jhadermoreno@hotmail.com>
2022-06-08 16:21:10 -0500

    PR comments applied, changes applied to user model

---
+++ db/migrate/20220608201246_add_details_to_user.rb
@@ -0,0 +1,8 @@
+class AddDetailsToUser < ActiveRecord::Migration[7.0]
+  def change
+    change_table :users do
+      change_column_null :users, :name, false
+      change_column_null :users, :keywords, false
+    end
+  end
+end
