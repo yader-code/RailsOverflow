42e4af33a057a80748652058c327067b5f7c480d
yader-code <jhadermoreno@hotmail.com>
2022-06-10 14:31:29 -0500

    post and comment model added

---
+++ db/migrate/20220610190941_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :content
+      t.string :string
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
