d582e8cf0848161c2befb8351e7e798c93101b80
yader-code <jhadermoreno@hotmail.com>
2022-07-19 14:31:33 -0500

    rubocop error fixed

--- app/controllers/filter_and_pagination.rb
+++ app/controllers/filter_and_pagination.rb
@@ -1,6 +1,6 @@
+# frozen_string_literal: true
+
 module FilterAndPagination
-
-
   def get_paginated_and_filtered(model)
     model.order(query_params[:order_by]).page(query_params[:page]).per(query_params[:page_size])
   end
@@ -8,5 +8,4 @@ module FilterAndPagination
   def query_params
     params.permit(:page, :page_size, :order_by)
   end
-
 end
